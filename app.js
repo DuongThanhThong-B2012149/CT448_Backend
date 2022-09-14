51c7a740669921625f7dbd8d80beb16286025b65
thanhthong <thongduong61134@gmail.com>
2022-09-15 06:48:52 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -13,5 +14,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+// Handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
